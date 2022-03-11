fd3ef07d758700bda25fd67397c6192ef7bdf991
Vicente F <vfonfria@blueboot.com>
2022-03-11 01:38:25 -0300

    10

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
- ], function (Controller,MessageToast,JSONModel) {
-    "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-        onInit : function () {
-            // set data model on view
-            var oData = {
-               recipient : {
-                  name : "World"
-               }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-         },
-       onShowHello : function () {
-        MessageToast.show("Hello World");
-
-       }
-    });
- });
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+   "use strict";
+   return Controller.extend("root.controller.App", {
+      onShowHello : function () {
+         // read msg from i18n model
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+         var sRecipientName = this.getView().getModel().getProperty("/recipient/name");
+         var sRecipientLastName = this.getView().getModel().getProperty("/recipient/lastName");
+         var sMsg = oBundle.getText("helloMsg", [sRecipientName,sRecipientLastName]);
+         // show message
+         MessageToast.show(sMsg);
+      }
+   });
+});
